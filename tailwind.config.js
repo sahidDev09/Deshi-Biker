5a45669b2d3d14de44728fc4877395d6a9108662
MD SAHID AHMED <iamsahidofficial99@gmail.com>
2024-01-21 02:11:16 +0600

    first commit of deshi biker

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./scr/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  //...
+  plugins: [require("daisyui")],
+};
